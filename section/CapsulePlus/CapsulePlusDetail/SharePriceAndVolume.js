7d757a2b814deb5c9d9aeb4cb7b0acf535290054
GovindSahu123 <govind.sahu@valuecreatives.com>
2024-06-07 12:17:24 +0530

    delete demo chart

--- section/CapsulePlus/CapsulePlusDetail/SharePriceAndVolume.js
+++ section/CapsulePlus/CapsulePlusDetail/SharePriceAndVolume.js
@@ -14,7 +14,6 @@ const SharePriceAndVolume = ({ headingLabel = "", prices = [] }) => {
             />
             <NseLineChart prices={prices} />
             <VolumeBarChart prices={prices} />
-            {/* <SharePriceVolumeChart /> */}
         </div>
     )
 }
