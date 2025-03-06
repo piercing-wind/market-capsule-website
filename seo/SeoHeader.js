144afe91a941166e5576d058f73a63baf70d3801
piercing-wind <sourabhsharma5677@gmail.com>
2025-03-06 19:44:23 +0530

    Twitter Pixel

--- seo/SeoHeader.js
+++ seo/SeoHeader.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import Script from "next/script";
 import React from "react";
 const SeoHeader = ({ title = 'Market Capsule', description = 'Market Capsule', imageUrl = `/assests/capsule-plus/bolt.svg`, backUrl }) => {
     const fullUrl = process.env.WEB + backUrl;
     return (
+      <>
         <Head>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -34,6 +36,20 @@ const SeoHeader = ({ title = 'Market Capsule', description = 'Market Capsule', i
             <link rel="apple-touch-icon-precomposed" href="/assests/meta/brand-logo32.svg" />
             <link rel='canonical' href={fullUrl} />
         </Head>
+            <Script
+                id="twitter-pixel"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+                        },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+                        a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+                        twq('config','p84d1');
+                        twq('event', 'tw-p84d1-p84d2', {});
+                    `,
+                }}
+            />
+         </>
     )
 }
 export default SeoHeader;
